2de22f0c9f194a504d2ea35960154ccda2271265
sheikh-mu <moizmoin34@gmail.com>
2021-10-10 11:52:34 +0500

    hello

---
+++ Components/LoginScreen.js
@@ -0,0 +1,12 @@
+import { StatusBar } from 'expo-status-bar'
+import React from 'react'
+import { View, Text } from 'react-native'
+
+export default function LoginScreen() {
+    return (
+        <View>
+            <StatusBar style="light" />
+            <Text>This is Login Screen</Text>
+        </View>
+    )
+}
